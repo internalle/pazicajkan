578ef5e25575556e787f5bac7849138ac42292fa
Slobodan <internalle@gmai.com>
2018-11-05 21:40:19 +0100

    innit commit

---
+++ app/src/main/java/mk/com/pazicajkan/pazicajkan/MainActivity.java
@@ -0,0 +1,13 @@
+package mk.com.pazicajkan.pazicajkan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
