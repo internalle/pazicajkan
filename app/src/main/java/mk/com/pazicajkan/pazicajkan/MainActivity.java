824ca3d8c92c6244ac8a2f3315935d32aa09b79b
Slobodan <internalle@gmai.com>
2018-11-05 23:15:16 +0100

    Facebook login scafolding added

--- app/src/main/java/mk/com/pazicajkan/pazicajkan/MainActivity.java
+++ app/src/main/java/mk/com/pazicajkan/pazicajkan/MainActivity.java
@@ -1,13 +1,59 @@
 package mk.com.pazicajkan.pazicajkan;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        loginButton = (LoginButton)findViewById(R.id.fb_login_button_id);
+        textView = (TextView)findViewById(R.id.text_info);
+        callbackManager = CallbackManager.Factory.create();
+        initFacebookSDK();
+    }
+
+    private void initFacebookSDK() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login success");
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                textView.setText("Login error");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
