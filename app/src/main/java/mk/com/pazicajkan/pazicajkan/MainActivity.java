8c4e8265e0016aac5b301eeae4f929b4ea095267
Slobodan <internalle@gmai.com>
2018-11-18 18:59:06 +0100

    Login with facebook first implementation

--- app/src/main/java/mk/com/pazicajkan/pazicajkan/MainActivity.java
+++ app/src/main/java/mk/com/pazicajkan/pazicajkan/MainActivity.java
@@ -4,19 +4,41 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import mk.com.pazicajkan.pazicajkan.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     LoginButton loginButton;
     TextView textView;
     CallbackManager callbackManager;
+    String TAG = "PAZICAJKAN";
+
+    NetworkUtils networkUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +47,68 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    public void buttonClickedHandle(View button) {
+        Log.e(TAG, "BUTTON clicked");
+
+    }
+
     private void init() {
         loginButton = (LoginButton)findViewById(R.id.fb_login_button_id);
         textView = (TextView)findViewById(R.id.text_info);
         callbackManager = CallbackManager.Factory.create();
         initFacebookSDK();
+        networkUtils = new NetworkUtils(getResources().getString(R.string.api_base_url));
+
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends", "name"));
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+
+        if (isLoggedIn) {
+            GraphRequest request = GraphRequest.newMeRequest(
+                    accessToken,
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(
+                                JSONObject object,
+                                GraphResponse response) {
+
+                            try {
+                                String url  = getResources().getString(R.string.api_base_url) + "check-and-login";
+
+                                Map<String, String> data = new HashMap<String, String>();
+                                data.put("email", response.getJSONObject().getString("email"));
+                                data.put("name", response.getJSONObject().getString("name"));
+
+                                networkUtils.post(url, data, getApplicationContext());
+                            } catch (Exception $e) {
+
+                            }
+
+
+
+                            Log.e(TAG, "BEFORE REPONSE");
+                        }
+                    });
+
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,link,email,first_name,last_name,name");
+            request.setParameters(parameters);
+            request.executeAsync();
+
+          //  Log.e(TAG, "REQUEST SENT");
+        }
     }
 
     private void initFacebookSDK() {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                Log.e(TAG, loginResult.toString());
                 textView.setText("Login success");
+
             }
 
             @Override
---
+++ app/src/main/java/mk/com/pazicajkan/pazicajkan/utilities/NetworkUtils.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package mk.com.pazicajkan.pazicajkan.utilities;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import mk.com.pazicajkan.pazicajkan.R;
+
+/**
+ * These utilities will be used to communicate with the network.
+ */
+public class NetworkUtils {
+
+    String API_BASE_URL = "";
+    final static String PARAM_QUERY = "q";
+
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    public NetworkUtils(String urlString) {
+        API_BASE_URL = urlString;
+    }
+
+    /**
+     * Builds the URL used to query GitHub.
+     *
+     * @param githubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the GitHub.
+     */
+    public URL buildUrl(String githubSearchQuery) {
+        Uri builtUri = Uri.parse(API_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public void post(String url, Map map, Context context) {
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        Log.e("HERE", "WE GOT SOMETHING");
+        try {
+            JSONObject jsonBody = new JSONObject();
+
+            jsonBody.put("email", map.get("email"));
+            jsonBody.put("name", map.get("name"));
+
+            JsonObjectRequest stringRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    jsonBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.e("NEW", response.toString());
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("NEW", error.toString());
+                        }
+                    }
+            );
+
+            requestQueue.add(stringRequest);
+        } catch (Exception e) {
+
+        }
+    }
+}
